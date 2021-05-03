47c0cc774eb9b3f4fd5d11d1d76f1f30cf1a3c50
kumar <aravind111reddy@gmail.com>
2021-05-03 07:16:59 +0530

    updated from local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}} kishore</div>`,
+  template: `<div>Hello {{value}} kishore updated from local</div>`,
 })
 export class AppComponent {
   value = 'World';
